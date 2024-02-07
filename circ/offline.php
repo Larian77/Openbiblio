2eab29fd97b3ba90147614849e6710071e4fa208
Marcus Bautze <marcus@mbautze.de>
2024-02-07 13:45:01 +0100

    Solved Bug https://github.com/Larian77/Openbiblio/issues/2
    Signed-off-by: Marcus Bautze <marcus@mbautze.de>

--- circ/offline.php
+++ circ/offline.php
@@ -1,13 +1,13 @@
 <?php
-/* This file is part of a copyrighted work; it is distributed with NO WARRANTY.
+/*
+ * This file is part of a copyrighted work; it is distributed with NO WARRANTY.
  * See the file COPYRIGHT.html for more details.
  */
-
-require_once("../shared/common.php");
-require_once("../classes/Date.php");
-require_once("../classes/Form.php");
-require_once("../classes/CircQuery.php");
-require_once("../classes/Localize.php");
+require_once ("../shared/common.php");
+require_once ("../classes/Date.php");
+require_once ("../classes/Form.php");
+require_once ("../classes/CircQuery.php");
+require_once ("../classes/Localize.php");
 
 $tab = 'circulation';
 $nav = "offline";
@@ -15,7 +15,7 @@ $nav = "offline";
 $focus_form_name = "offline_circ";
 $focus_form_field = "date";
 
-require_once("../shared/logincheck.php");
+require_once ("../shared/logincheck.php");
 $loc = new Localize(OBIB_LOCALE, $tab);
 
 function run_batch($lines, $date)
@@ -27,44 +27,50 @@ function run_batch($lines, $date)
         $command = trim(array_shift($lines));
         if ($command == '')
             continue;
-        //Changes PVD(8.0.x)
+        // Changes PVD(8.0.x)
         if ($command[0] != '%')
-        return array($loc->getText("Bad upload file: Expected a command code, but didn't get one"));
-    $args = array();
-    //Changes PVD(8.0.x)
-        while (isset($lines[0]) and $lines[0] [ 0] != '%')
+            return array(
+                $loc->getText("Bad upload file: Expected a command code, but didn't get one")
+            );
+        $args = array();
+        // Changes PVD(8.0.x)
+        while (isset($lines[0]) and $lines[0][0] != '%')
             $args[] = trim(array_shift($lines));
         switch ($command) {
             case '%CHECKOUT%':
-                if (!isset($args[0]))
+                if (! isset($args[0]))
                     break;
                 $mbcode = array_shift($args);
                 foreach ($args as $bcode) {
                     $err = $circQ->checkout_as_of_e($mbcode, $bcode, $date);
                     if ($err !== NULL)
-                        $errors[] = $loc->getText(
-                            "Couldn't check out %item% to %member%: %error%",
-                            array('item' => $bcode, 'member' => $mbcode, 'error' => $err->toStr())
-                        );
+                        $errors[] = $loc->getText("Couldn't check out %item% to %member%: %error%", array(
+                            'item' => $bcode,
+                            'member' => $mbcode,
+                            'error' => $err->toStr()
+                        ));
                 }
                 break;
             case '%CHECKIN%':
                 foreach ($args as $bcode) {
-                    list($info, $err) = $circQ->shelving_cart_e($bcode, $date, true);
+                    list ($info, $err) = $circQ->shelving_cart_e($bcode, $date, true);
                     if ($err)
-                        $errors[] = $loc->getText(
-                            "Couldn't check in %item%: %error%",
-                            array('item' => $bcode, 'error' => $err->toStr())
-                        );
+                        $errors[] = $loc->getText("Couldn't check in %item%: %error%", array(
+                            'item' => $bcode,
+                            'error' => $err->toStr()
+                        ));
                 }
                 break;
             default:
-                $errors[] = $loc->getText("Unrecognized command code: %cmd%", array('cmd' => H($command)));
+                $errors[] = $loc->getText("Unrecognized command code: %cmd%", array(
+                    'cmd' => H($command)
+                ));
                 break;
         }
     }
     return $errors;
 }
+
 function layout_links()
 {
     global $loc;
@@ -78,34 +84,50 @@ $form = array(
     'enctype' => 'multipart/form-data',
     'submit' => $loc->getText('Upload'),
     'fields' => array(
-        array('name' => 'date', 'title' => $loc->getText('Date:'), 'type' => 'date', 'default' => 'today'),
-        array('name' => 'command_file', 'title' => $loc->getText('Command File:'), 'type' => 'file', 'required' => 1),
-    ),
+        array(
+            'name' => 'date',
+            'title' => $loc->getText('Date:'),
+            'type' => 'date',
+            'default' => 'today'
+        ),
+        array(
+            'name' => 'command_file',
+            'title' => $loc->getText('Command File:'),
+            'type' => 'file',
+            'required' => 1
+        )
+    )
 );
-//Changes PVD(8.0.x)
-list($values, $errs) = (new Form)->getCgi_el($form['fields']);
-if (!$values['_posted'] or $errs) {
-    include_once("../shared/header.php");
+// Changes PVD(8.0.x)
+list ($values, $errs) = (new Form())->getCgi_el($form['fields']);
+if (! $values['_posted'] or $errs) {
+    include_once ("../shared/header.php");
     if (isset($_REQUEST['msg'])) {
         echo '<font class="error">' . H($_REQUEST['msg']) . '</font>';
     }
     $form['values'] = $values;
     $form['errors'] = $errs;
-    //Changes PVD(8.0.x)
-    (new Form)->display($form);
+    // Changes PVD(8.0.x)
+    (new Form())->display($form);
     layout_links();
-    include_once("../shared/footer.php");
+    include_once ("../shared/footer.php");
     exit();
 }
 
-$lines = file($values['command_file']['tmp_name']);
+if (file_exists($values['command_file']['tmp_name']))
+    $lines = file($values['command_file']['tmp_name']);
+else
+    $lines = false;
 if ($lines === false)
-    $errors = array("Couldn't read file: " . $values['command_file']['tmp_name']);
+    $errors = array(
+        $loc->getText("Couldn't read file: ") . $values['command_file']['tmp_name']
+    );
 else
     $errors = run_batch($lines, $values['date']);
 if ($errors) {
-    include_once("../shared/header.php");
-    echo '<font class="error">' . $loc->getText("Actions which did not produce an error have completed. Think carefully before uploading the same file again, or some circulations may be recorded twice.") . '</font>';
+    include_once ("../shared/header.php");
+    if ($lines != false)
+        echo '<font class="error">' . $loc->getText("Actions which did not produce an error have completed. Think carefully before uploading the same file again, or some circulations may be recorded twice.") . '</font>';
     echo '<div class="errorbox">';
     echo '<span class="errorhdr">' . $loc->getText('Errors') . '</span>';
     echo '<ul>';
@@ -113,10 +135,10 @@ if ($errors) {
         echo '<li>' . H($e) . '</li>';
     }
     echo '</ul></div>';
-    //Changes PVD(8.0.x)
-    (new Form)->display($form);
+    // Changes PVD(8.0.x)
+    (new Form())->display($form);
     layout_links();
-    include_once("../shared/footer.php");
+    include_once ("../shared/footer.php");
     exit();
 } else
     header("Location: ../circ/offline.php?msg=" . U($loc->getText("Offline Upload Successful.")));
