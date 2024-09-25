51bce2324b1e7de6db085a29bedd7e6095c30fba
sarangtc <sarangtc@gmail.com>
2024-09-25 16:34:50 +0530

    git ignore db constants

--- database_constants.php
+++ database_constants.php
@@ -7,11 +7,11 @@
  *  \/  MySQL Datenbank und dem MySQL-Benutzer, den Sie erstellt haben.     \/
  *********************************************************************************
  */
-define("OBIB_HOST",     "localhost");
-define("OBIB_DATABASE", "openbiblio");
-define("OBIB_USERNAME", "db_user");
-define("OBIB_PWD",      "db_passw");
-define("MAIN_LOCALE",   "de"); // de oder en
+define("OBIB_HOST",     "192.168.0.47");
+define("OBIB_DATABASE", "aarshopenbiblio");
+define("OBIB_USERNAME", "param");
+define("OBIB_PWD",      "param");
+define("MAIN_LOCALE",   "en"); // de oder en
 /*********************************************************************************
  *  /\                                                                      /\
  *  ||                                                                      ||
